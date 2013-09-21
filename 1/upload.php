6d2a184a1525fb71b85084e4b13c862383235dd0
Cheney <me@chenye.org>
2013-09-21 23:44:05 +0800

    update ajax upload

--- 1/upload.php
+++ 1/upload.php
@@ -2,20 +2,41 @@
 include "db.php";
 include "upload-lib.php";
 
+
+
 $info = mustGetAuthorizationInfo();
 $timeline_policy->EndUser = $info['uid'];
-$timeline_policy->ReturnUrl = "http://qiniutimeline.sinaapp.com/upload-api.php";
+#$timeline_policy->ReturnUrl = "http://qiniutimeline.sinaapp.com/upload-api.php";
+header("Content-Type: text/html; charset=UTF-8");
 ?>
 <html>
     <head>
         <title>upload</title>
     </head>
     <body>
-		<form method="post" action="http://up.qiniu.com/" enctype="multipart/form-data">
+        <form id="fileinfo" method="post" action="http://up.qiniu.com/" enctype="multipart/form-data">
             <input type="file" name="file" /><br />
             <textarea name="x:desc" rows=10></textarea><br />
             <input type="hidden" name="token" value="<?=$timeline_policy->Token(null)?>" />
             <input type="submit" />
+
         </form>
+        <button id="upload">ajax上传</button>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>
+            $("#upload").click(function() {
+            	var formData = new FormData(document.getElementById('fileinfo'));
+                jQuery.ajax("http://up.qiniu.com/", {
+                    processData: false,
+                    contentType: false,
+                    type: "POST",
+                    data: formData,
+                    success: function(ret) {
+						alert(JSON.stringify(ret));
+                    }
+                });
+            });
+        </script>
+            
     </body>
 </html>
